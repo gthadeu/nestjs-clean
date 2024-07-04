76b5ed9dd02ebecd4cb795397a5cfc2f88a35b59
Thadeu <thadeu.qodeless@outlook.com>
2024-07-04 19:59:49 -0300

    test e2e questions

---
+++ src/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -0,0 +1,73 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Fetch questions (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  //* subir a aplicação de forma pragmatica
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+    await app.init()
+  })
+
+  test('[GET] /questions', async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'Teste',
+        email: 'teste@teste.com',
+        password: '123456',
+      },
+    })
+
+    const accessToken = jwt.sign({ sub: user.id })
+
+    await prisma.question.createMany({
+      data: [
+        {
+          title: 'question 01',
+          slug: 'question-01',
+          content: 'question content',
+          authorId: user.id,
+        },
+        {
+          title: 'question 02',
+          slug: 'question-02',
+          content: 'question content',
+          authorId: user.id,
+        },
+        {
+          title: 'question 03',
+          slug: 'question-03',
+          content: 'question content',
+          authorId: user.id,
+        },
+      ],
+    })
+
+    const response = await request(app.getHttpServer())
+      .get('/questions')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      questions: [
+        expect.objectContaining({ title: 'question 01' }),
+        expect.objectContaining({ title: 'question 02' }),
+        expect.objectContaining({ title: 'question 03' }),
+      ],
+    })
+  })
+})
