810a702f2bfbf0638213432d9da6e1e8fc0101a2
Thadeu <thadeu.qodeless@outlook.com>
2024-07-22 19:03:30 -0300

    list recent question

--- src/domain/forum/application/use-cases/fetch-recent-questions.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -1,6 +1,7 @@
-import { Question } from '@/domain/forum/enterprise/entities/question'
-import { QuestionsRepository } from '../repositories/questions-repository'
 import { Either, right } from '@/core/either'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Injectable } from '@nestjs/common'
+import { QuestionsRepository } from '../repositories/questions-repository'
 
 interface FetchRecentQuestionsUseCaseRequest {
   page: number
@@ -12,7 +13,7 @@ type FetchRecentQuestionsUseCaseResponse = Either<
     questions: Question[]
   }
 >
-
+@Injectable()
 export class FetchRecentQuestionsUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
